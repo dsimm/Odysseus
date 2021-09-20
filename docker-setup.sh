7fea8cd96aa80c55a51fe96f0fac7fec5ef37e2f
Dominic Simm <dominic.simm@cs.uni-goettingen.de>
2021-09-20 22:15:52 +0200

    Improve setup scripts

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# Extract biological data from packed archive
+[ -d 'bio_data' ] && tar xfz bio_data.tar.gz
+
 # Build docker image of project with composer-preferences
 docker-compose build
-# docker-compose build --no-cache --force-rm
+# docker-compose -f docker-compose.yml build --no-cache
 
 # Start image as container via composer
 # docker-compose up
